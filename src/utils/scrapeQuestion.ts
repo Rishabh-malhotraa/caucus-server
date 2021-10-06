3edead9b657b08cdb3a75ee46a9fcb90c2274429
Kundan <starkkumarkk1611@gmail.com>
2021-10-06 14:06:34 +0530

    CSES problem url Scrapping feature added

--- src/utils/scrapeQuestion.ts
+++ src/utils/scrapeQuestion.ts
@@ -36,7 +36,29 @@ export const scrapeQuestion = async (url: string, hostname: string): Promise<Ret
         htmlString: "",
       };
     }
-  } else {
+  } else if (hostname === "cses.fi") {
+    try {
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+      $('.skeleton .nav').remove();  //removes all nav on cses page
+      $('.title-block > h3').remove(); // removes cses problem set link 
+      $('script').remove(); // removes all the scripts 
+
+
+
+      const questionData = $('.skeleton').html();
+      return questionData === null ? { error: true, htmlString: "" } : { error: false, htmlString: questionData };
+
+    } catch (err) {
+      return {
+        error: true,
+        htmlString: "",
+      };
+
+    }
+
+  }
+  else {
     return {
       error: true,
       htmlString: "",
